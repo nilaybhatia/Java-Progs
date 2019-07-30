2c0d28cb21717a043cffaa0fc4011495dc42e387
nilaybhatia <bhatianilay@gmail.com>
2019-07-30 06:59:29 +0000

    New prac questions

---
+++ Q4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    Scanner sc = new Scanner(System.in);
+    
+    sc.close();
+  }
+}
+
+class Library {
+  String name;
+  long acc_no;
+  int books_issued;
+  double fine;
+
+}
