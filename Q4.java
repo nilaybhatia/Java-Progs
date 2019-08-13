37756707c50f684ceab88380959ccc1d1722da1c
nilaybhatia <bhatianilay@gmail.com>
2019-08-13 23:01:02 +0530

    Q4 almost done

--- Q4.java
+++ Q4.java
@@ -15,7 +15,7 @@ class Library
         fine = 0.0;
     }
     public void issueBook()
-    {	
+    {
         System.out.println("Enter accession number of the book");
         int acc_no = sc.nextInt();
         books_issued[i] = acc_no;
@@ -24,7 +24,7 @@ class Library
 
     }
     public void returnBook()
-    {	
+    {
         System.out.println("Enter accession number of the book");
         int acc_no = sc.nextInt();
         int j;
@@ -48,36 +48,59 @@ class Library
 }
 
 class Main
-{	
+{
     public static void main(String args[])
     {
-        Library account = new Library();
         Scanner sc = new Scanner(System.in);
-        int choice;
-        do{
-          System.out.println("Enter a choice");
-          System.out.println("1. Issue a book\n2. Return a book\n3. Charge fine\n4. Display fine\n5. Exit");
-          choice = sc.nextInt();
-          switch(choice)    
-          {   
-            case 1:
-            account.issueBook();
-            break;
+        System.out.println("Enter the number of students");
+        int n = sc.nextInt();
+        Library stud[] = new Library[n];
+        for(int i = 0; i < n; i++) {
+            int choice;
+            stud[i] = new Library();
+            System.out.println("Student " + (i+1));
+            System.out.println("Enter name");
+            stud[i].name = sc.next();
+            String dummy = sc.next();
+            System.out.println("Enter account number");
+            stud[i].acc_no = sc.nextLong();
+            do{
+                System.out.println("Enter a choice");
+                System.out.println("1. Issue a book\n2. Return a book\n3. Charge fine\n4. Exit for this student");
+                choice = sc.nextInt();
+                System.out.println(choice);
+                switch(choice)
+                {
+                    case 1:
+                    stud[i].issueBook();
+                    break;
 
-            case 2:
-            account.returnBook();
-            break;
+                    case 2:
+                    stud[i].returnBook();
+                    break;
 
-            case 3:
-            account.chargeFine();
-            break;
-
-            case 4:
-            sortByFine()
-            break;
-          }
-        }while(choice != 5);
-        account.issueBook();
+                    case 3:
+                    stud[i].chargeFine();
+                    break;
+                }
+            }while(choice != 4);
+        }
+        System.out.println("The list of students sorted by fine is");
+        sortByFine(stud);
+        System.out.println("Account number\t\t\tName\t\t\tFine");
+        for(int i = 0; i < stud.length; i++){
+            System.out.println(stud[i].acc_no + "\t\t" + stud[i].name + "\t\t" + stud[i].fine);
+        }        
+    }
+    public static void sortByFine(Library students[]){
+        for(int i = 1; i < students.length; i++){
+            for(int j = 0; j < students.length-1; j++)
+                if(students[j].fine < students[j+1].fine){
+                    Library temp = students[j];
+                    students[j] = students[j+1];
+                    students[j+1] = temp;
+                }
+        }
     }
 }
-     
+
