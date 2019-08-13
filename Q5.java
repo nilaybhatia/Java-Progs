e26c2e2dd92d56908ead2b143db2acac3db30dd1
nilaybhatia <bhatianilay@gmail.com>
2019-08-13 17:20:25 +0530

    Q6 done

---
+++ Q5.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Shape
+{	
+	double volume;
+	Shape(double side)
+	{
+		volume = side*side*side; 
+	}
+	Shape(double length, double breadth, double height)
+	{
+		volume = length*breadth*height;
+	}
+}
+class Q5
+{	
+	public static void main(String args[])
+	{
+		Shape cube = new Shape(10.2);
+		System.out.println("The volume of the cube is " + cube.volume);
+		Shape cuboid = new Shape(10, 20, 30);
+		System.out.println("The volume of the cuboid is " + cuboid.volume);
+
+	}
+}
+/*Remeber to keep data type of volume as double*/
---
+++ Q7.java
@@ -0,0 +1,24 @@
+class Test
+{
+	int a, b;	
+	Test(int a, int b)
+	{
+		this.a = a;
+		this.b = b;
+	}	
+	public static Test func(Test o)
+	{
+		o.a *= 2;
+		o.b += 4;
+		return o;
+	}
+	public static void main(String args[])
+	{
+		Test ob = new Test(2, 3);
+		Test ob1 = new Test(0, 0);
+		System.out.println("Object's attributes before calling function: ob. a = " + ob.a + " and ob.b = " + ob.b);
+		ob1 = ob.func(ob);	
+		System.out.println("Object's attributes after calling function: ob. a = " + ob.a + " and ob.b = " + ob.b);
+		System.out.println("Object's attributes after calling function: ob1. a = " + ob1.a + " and ob1.b = " + ob1.b);
+	}
+}
