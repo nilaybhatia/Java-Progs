7b965253d8879987deaff2c1daf7530b25fc733a
nilaybhatia <bhatianilay@gmail.com>
2019-08-18 07:19:10 +0000

    Q8

---
+++ Q8.java
@@ -0,0 +1,9 @@
+import MyPackage.*;
+class Q8{
+  public static void main(String args[]){
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    System.out.println("The array elements are");
+    Array ob = new Array();
+    ob.printArray(arr);
+  } 
+}
