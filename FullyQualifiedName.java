d1d6609a551bd22817baa89dd7b662f9105e4bf5
nilaybhatia <bhatianilay@gmail.com>
2019-08-27 05:25:22 +0530

    Variations

---
+++ FullyQualifiedName.java
@@ -0,0 +1,10 @@
+public class FullyQualifiedName
+{
+	public static void main(String args[])
+	{
+		java.lang.System.out.println("This message is printed without importing any package and using only fully Qualified Name");
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		java.lang.System.out.println("Enter a number");
+		java.lang.System.out.println(sc.nextInt());
+	}
+}
---
+++ ImportAnotherPackage.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Q
+{
+	public static void main(String args[])
+	{
+		System.out.println("");
+	}
+}
